ce741266d85ed75b2e174797da8ebbfe429db369
long. <dailong581@gmail.com>
2025-09-09 19:28:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// route chính
 app.get("/", (req, res) => {
   res.send("Xin chào từ streaky ✨");
 });
 
+// lắng nghe server
 app.listen(PORT, () => {
   console.log(✅ streaky đang chạy ở cổng ${PORT});
 });
