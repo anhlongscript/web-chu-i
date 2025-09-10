78b67527d5941880958f06b28d412cc4d5056447
long. <dailong581@gmail.com>
2025-09-10 12:13:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// index.js
+// index.js (root của repo)
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Cho phép Express phục vụ file tĩnh trong thư mục public
+// serve thư mục public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route mặc định -> index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ streaky đang chạy ở cổng ${PORT}`);
+  console.log(`✅ Streaky đang chạy ở cổng ${PORT}`);
 });
