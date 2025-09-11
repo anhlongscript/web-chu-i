d30b116a39e0be9376dd74bf50cf17c31d56bdef
long. <dailong581@gmail.com>
2025-09-11 17:41:42 +0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,37 +1,67 @@
 const socket = io();
 let nickname = "";
 let avatar = "";
+let password = "";
 
 // Convert ảnh thành base64
 function getBase64(file, callback) {
   const reader = new FileReader();
-  reader.onload = function () {
-    callback(reader.result);
-  };
+  reader.onload = () => callback(reader.result);
   reader.readAsDataURL(file);
 }
 
-function joinChat() {
-  nickname = document.getElementById("nickname").value.trim();
-  const file = document.getElementById("avatarFile").files[0];
+// Fake Database trong localStorage
+let usersDB = JSON.parse(localStorage.getItem("usersDB") || "{}");
 
-  if (!nickname) return alert("Bạn cần nhập nickname!");
+function register() {
+  const nick = document.getElementById("regNick").value.trim();
+  const pass = document.getElementById("regPass").value.trim();
+  const file = document.getElementById("regAvatar").files[0];
+
+  if (!nick || !pass) return alert("Vui lòng nhập đầy đủ thông tin!");
+
+  if (usersDB[nick]) return alert("Nickname đã tồn tại!");
 
   if (file) {
     getBase64(file, (data) => {
-      avatar = data;
-      startChat();
+      usersDB[nick] = { password: pass, avatar: data };
+      localStorage.setItem("usersDB", JSON.stringify(usersDB));
+      alert("Đăng ký thành công! Hãy đăng nhập.");
+      switchToLogin();
     });
   } else {
-    avatar = "https://i.pravatar.cc/150?u=" + nickname; // avatar mặc định
-    startChat();
+    const defaultAvatar = "https://i.pravatar.cc/150?u=" + nick;
+    usersDB[nick] = { password: pass, avatar: defaultAvatar };
+    localStorage.setItem("usersDB", JSON.stringify(usersDB));
+    alert("Đăng ký thành công! Hãy đăng nhập.");
+    switchToLogin();
   }
 }
 
-function startChat() {
-  document.getElementById("login").style.display = "none";
+function login() {
+  const nick = document.getElementById("loginNick").value.trim();
+  const pass = document.getElementById("loginPass").value.trim();
+
+  if (!usersDB[nick]) return alert("Tài khoản không tồn tại!");
+  if (usersDB[nick].password !== pass) return alert("Sai mật khẩu!");
+
+  nickname = nick;
+  avatar = usersDB[nick].avatar;
+
+  document.getElementById("auth").style.display = "none";
   document.getElementById("chat").style.display = "flex";
+
   socket.emit("join", { nickname, avatar });
+}
+
+function switchToLogin() {
+  document.getElementById("registerBox").style.display = "none";
+  document.getElementById("loginBox").style.display = "block";
+}
+
+function switchToRegister() {
+  document.getElementById("registerBox").style.display = "block";
+  document.getElementById("loginBox").style.display = "none";
 }
 
 function sendMessage() {
@@ -43,18 +73,22 @@ function sendMessage() {
   }
 }
 
-// Nhận tin nhắn từ server
+// Nhận tin nhắn
 socket.on("chatMessage", (data) => {
   const messages = document.getElementById("messages");
   const div = document.createElement("div");
   div.className = "message";
-  div.innerHTML = `<img src="${data.avatar}" class="avatar">
-                   <b>${data.nickname}:</b> ${data.text}`;
+  div.innerHTML = `
+    <img src="${data.avatar}" class="avatar">
+    <div>
+      <b>${data.nickname}</b><br>
+      <span>${data.text}</span>
+    </div>`;
   messages.appendChild(div);
   messages.scrollTop = messages.scrollHeight;
 });
 
-// Cập nhật danh sách user online
+// Cập nhật user online
 socket.on("updateUsers", (users) => {
   const userList = document.getElementById("userList");
   const count = document.getElementById("count");
@@ -62,8 +96,7 @@ socket.on("updateUsers", (users) => {
   count.innerText = Object.keys(users).length;
   for (let id in users) {
     const li = document.createElement("li");
-    li.innerHTML = `<img src="${users[id].avatar}" class="avatar">
-                    ${users[id].nickname}`;
+    li.innerHTML = `<img src="${users[id].avatar}" class="avatar"> ${users[id].nickname}`;
     userList.appendChild(li);
   }
 });
